8f29124893bfc0b7ffe69049640bc96fadea01c4
aghniadm <aghniadewii@gmail.com>
2020-10-19 08:52:49 +0700

    PBO2-10119073-Latihan9-Teks

---
+++ src/pbo2/pkg10119073/latihan9/teks/PBO210119073Latihan9Teks.java
@@ -0,0 +1,22 @@
+package pbo2.pkg10119073.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan teks
+ *
+ */
+public class PBO210119073Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+    
+}
